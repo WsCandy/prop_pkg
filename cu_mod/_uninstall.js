1873f03848017838aab6e6f4c9108aaf8baeaf95
Samuel - Work <s-bridge@live.co.uk>
2014-06-16 16:19:32 +0100

    [_uninstall] - added module

---
+++ cu_mod/_uninstall.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var self = this;
+
+var colour = require('cli-color'),
+    notice = colour.yellow,
+    error = colour.red;
+
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
+var pp_move = require('./_move');
+
+self.uninstall = function() {
+
+
+
+}
