27332f2e55aa783a23e0f15e1ea144429165172f
Samuel - Work <s-bridge@live.co.uk>
2014-06-16 10:28:12 +0100

    [Modules] - Module restructure

---
+++ cu_mod/_install.js
@@ -0,0 +1,113 @@
+'use strict';
+
+var self = this;
+
+var colour = require('cli-color'),
+    notice = colour.yellow,
+    error = colour.red;
+
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
+var pp_move = require('./_move');
+
+var install_count = 0;
+
+self.loop_install = function() {
+
+    if(global.results_info.pkgMeta['install']) {
+
+        self.filesObj = global.results_info.pkgMeta['install'];
+        console.log(notice('\nCopying Files...\n'));
+
+        for(var key in self.filesObj) {
+
+            self.create_dirs(key, self.filesObj[key]);
+
+        }
+
+    } else {
+
+        global.filesObj = global.results_info.pkgMeta['main'];
+        var file = global.results_info.pkgMeta['main'];
+
+        if(file) {
+            
+            console.log(notice('\nInstall paths not defined. Moving the "main" file into the correct directory... I hope...\n'));
+
+            if(typeof file == 'string') {
+
+                self.create_dirs(file, self.derive_install_path(self.derive_file_type(file)));
+                
+            } else {
+
+                for (var i = 0; i < file.length; i++) {
+
+                    self.create_dirs(file[i], self.derive_install_path(self.derive_file_type(file[i])));
+
+                }
+
+            }
+            
+        } else {
+
+            console.log(notice('\nLooks like the author of '+global.results_info.pkgMeta['name']+' couldn\'t be bothered to set the main file...! You\'re going to have to move files manually, sorry about that!'));
+            pp_move.complete_log();
+
+        }
+
+    }
+
+}
+
+self.create_dirs = function(file, install_loc) {
+
+    mkdirp(global.install_dir+'/../../'+install_loc, '0777', function(err) {
+
+        if(err) throw err;
+
+        self.move_file(file, install_loc);
+        
+    }); 
+
+}
+
+self.move_file = function(file, path) {
+
+    var copy_file = fs.createReadStream(global.install_dir+'/'+file);
+        copy_file.on('error', function(err) {
+
+            if(err) throw err;
+
+        });
+
+    file = file.split('/');
+    file = file[file.length -1];
+
+    var copied_file = fs.createWriteStream(global.install_dir+'/../../'+path+'/'+file);
+        copied_file.on('error', function(err){
+
+             if(err) throw err;
+
+        });
+
+    copied_file.on('close', function() {
+
+        console.log(notice(file + ' copied over to ' + path));
+        install_count++;
+
+        if(self.filesObj) {
+
+            if(install_count == (typeof self.filesObj == 'object' ? Object.keys(self.filesObj).length : (typeof self.filesObj == 'string' ? 1 : self.filesObj.length))) pp_move.complete_log();
+            
+        } else {
+
+            pp_move.complete_log();
+
+        }
+
+    });
+
+    copy_file.pipe(copied_file);
+
+}
--- cu_mod/_move.js
+++ cu_mod/_move.js
@@ -3,27 +3,25 @@
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 
+var pp_install = require('../cu_mod/_install');
+
 var colour = require('cli-color'),
     notice = colour.yellow,
     error = colour.red;
 
-var install_count = 0;
-
 var self = this;
 
 
-self.callback_handle = function(data) {
+self.callback_handle = function(data, command) {
 
-    // Set data to be global to this module
+    global.data = data;    
+    global.pkg_name = global.installed_package.split('#');
+    global.results_info = global.data[global.pkg_name[0]];
 
-    self.data = data;    
-    self.pkg_name = global.installed_package.split('#');
-    self.results_info = self.data[self.pkg_name[0]];
-
-    if(self.results_info) {
-        
-        self.install_dir = self.results_info['canonicalDir'];
-        self.loop_install();
+    if(global.results_info && command == 'install') {
+    
+        global.install_dir = global.results_info['canonicalDir'];
+        pp_install.loop_install();
         
     } else {
 
@@ -35,111 +33,12 @@ self.callback_handle = function(data) {
 
 self.complete_log = function() {
 
-    var user_defined = self.results_info.pkgMeta,
+    var user_defined = global.results_info.pkgMeta,
         package_name = user_defined['name'],
         author = user_defined['authors'],
-        version = self.results_info.endpoint['target'];
+        version = global.results_info.endpoint['target'];
     
     console.log(notice('\n'+package_name + ' ' + version + ' by ' + (author == undefined ? 'No Author :(' : author) + ' sucessfully installed!! Lucky you.\n'));
-
-}
-
-self.loop_install = function() {
-
-    if(self.results_info.pkgMeta['install']) {
-
-        self.filesObj = self.results_info.pkgMeta['install'];
-        console.log(notice('\nCopying Files...\n'));
-
-        for(var key in self.filesObj) {
-
-            self.create_dirs(key, self.filesObj[key]);
-
-        }
-
-    } else {
-
-        self.filesObj = self.results_info.pkgMeta['main'];
-        var file = self.results_info.pkgMeta['main'];
-
-        if(file) {
-            
-            console.log(notice('\nInstall paths not defined. Moving the "main" file into the correct directory... I hope...\n'));
-
-            if(typeof file == 'string') {
-
-                self.create_dirs(file, self.derive_install_path(self.derive_file_type(file)));
-                
-            } else {
-
-                for (var i = 0; i < file.length; i++) {
-
-                    self.create_dirs(file[i], self.derive_install_path(self.derive_file_type(file[i])));
-
-                }
-
-            }
-            
-        } else {
-
-            console.log(notice('\nLooks like the author of '+self.results_info.pkgMeta['name']+' couldn\'t be bothered to set the main file...! You\'re going to have to move files manually, sorry about that!'));
-            self.complete_log();
-
-        }
-
-    }
-
-}
-
-self.create_dirs = function(file, install_loc) {
-
-    mkdirp(self.install_dir+'/../../'+install_loc, '0777', function(err) {
-
-        if(err) throw err;
-
-        self.move_file(file, install_loc);
-        
-    }); 
-
-}
-
-self.move_file = function(file, path) {
-
-    var copy_file = fs.createReadStream(self.install_dir+'/'+file);
-        copy_file.on('error', function(err) {
-
-            if(err) throw err;
-
-        });
-
-    file = file.split('/');
-    file = file[file.length -1];
-
-    var copied_file = fs.createWriteStream(self.install_dir+'/../../'+path+'/'+file);
-        copied_file.on('error', function(err){
-
-             if(err) throw err;
-
-        });
-
-    copied_file.on('close', function() {
-
-        console.log(notice(file + ' copied over to ' + path));
-        install_count++;
-
-        if(self.filesObj) {
-
-            if(install_count == (typeof self.filesObj == 'object' ? Object.keys(self.filesObj).length : (typeof self.filesObj == 'string' ? 1 : self.filesObj.length))) self.complete_log();
-            
-        } else {
-
-            self.complete_log();
-
-        }
-
-    });
-
-    copy_file.pipe(copied_file);
 
 }
 
