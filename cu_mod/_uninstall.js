4e7a51e689130f6ddacd3069d463b1c1fd39f22a
Samuel - Work <s-bridge@live.co.uk>
2014-06-16 18:47:09 +0100

    [_uninstall] - Added remove files methods

--- cu_mod/_uninstall.js
+++ cu_mod/_uninstall.js
@@ -10,31 +10,61 @@ var fs = require('fs');
 
 var pp_move = require('./_move');
 
+var uninstall_count = 0;
+
 self.uninstall_handle = function(data) {
 
-	var installed_package,
-		install_path;
+	if(Object.keys(data).length) {
 
-	for(var key in data) {
+		var installed_package,
+			install_path;
 
-		installed_package = key;
-		install_path = data[key];
+		for(var key in data) {
 
-	}
+			installed_package = key;
+			install_path = data[key];
 
-	var bower_json = require(install_path+'/../../.bower-cache/'+installed_package+'/bower.json');
+		}
 
-	for (var key in bower_json.install) {
+		var bower_json = require(install_path+'/../../.bower-cache/'+installed_package+'/bower.json');
 
-		var file = key;
-		var dir = bower_json.install[key];
+		if(bower_json.install) {
+
+			self.filesObj = bower_json.install;
+
+			console.log(notice('\nInstall directories defined... Removing files from their directories!\n'));
+
+			for (var key in bower_json.install) {
+
+				var file = key;
+				var dir = bower_json.install[key];
+
+				self.remove_file(file, dir);
+
+			}
+			
+		}
+
+	} else {
+
+		console.log(error('\nThis package already appears to have been removed... erm... yeah... :(\n'));
 
 	}
 
 }
 
-self.remove_file = function(file, path) {
+self.remove_file = function(file, dir) {
 
+	fs.unlink(dir + '/' + file, function(err) {
 
+		if(err) throw err;
+
+		console.log(notice(file + ' removed from ' + dir));
+
+		uninstall_count++;
+		
+		if(uninstall_count == (typeof self.filesObj == 'object' ? Object.keys(self.filesObj).length : (typeof self.filesObj == 'string' ? 1 : self.filesObj.length))) pp_move.complete_log('uninstall');
+
+	});
 
 }
